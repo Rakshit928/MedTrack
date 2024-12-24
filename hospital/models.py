349b1f4ad34ce4f7bac400cd88463b04bdf36947
Rakshit928 <rakshitanand09@gmail.com>
2024-12-25 04:58:40 +0530

    commit

--- hospital/models.py
+++ hospital/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
+ 
 departments=[('Cardiologist','Cardiologist'),
 ('Dermatologists','Dermatologists'),
 ('Emergency Medicine Specialists','Emergency Medicine Specialists'),
